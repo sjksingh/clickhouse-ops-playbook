af3d63c8df28b4a113e6ea2aaa3209cf57403e39
Sanjeev Singh <sjksingh@gmail.com>
2025-10-29 17:47:25 -0700

    Update generate_cardinality_query.sql

--- cardinality_profiling/generate_cardinality_query.sql
+++ cardinality_profiling/generate_cardinality_query.sql
@@ -1,5 +1,4 @@
 -- Generates a query for all columns of a table
-SELECT 
-    'SELECT ' || groupConcat('uniq(' || name || ') AS uniq_' || name, ', ') || ', count() AS total_rows FROM ' || database || '.' || table AS query_text
+SELECT concat('SELECT ', groupConcat(concat('uniq(', name, ') AS uniq_', name), ', '), ', count() AS total_rows FROM observations.deduped_observations_lc') AS query_text
 FROM system.columns
-WHERE database = 'your_database' AND table = 'your_table';
+WHERE (database = 'database') AND (`table` = 'table');
