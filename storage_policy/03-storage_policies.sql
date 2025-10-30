15f0e49507346e6a05c2cc45bf052e3a3ec3ea6c
Sanjeev Singh <sjksingh@gmail.com>
2025-10-30 09:57:52 -0700

    Delete storage_policy/03-storage_policies.sql

--- storage_policy/03-storage_policies.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    '3. STORAGE POLICIES' as section,
-    storage_policy,
-    count() as tables_count,
-    formatReadableSize(sum(total_bytes)) as total_size
-FROM system.tables
-WHERE database NOT IN ('system', 'information_schema', 'INFORMATION_SCHEMA')
-GROUP BY storage_policy
-ORDER BY sum(total_bytes) DESC;
